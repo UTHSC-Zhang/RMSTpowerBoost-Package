f527d0f68fd4a6432e6eb6675916be06254c1983
Arnab Aich <118152677+arnabaich96@users.noreply.github.com>
2025-07-09 03:04:33 -0500

    added app

---
+++ R/run_app.R
@@ -0,0 +1,29 @@
+#' Launch the RMSTdesign Shiny Application
+#'
+#' @description
+#' A function to easily launch the interactive Shiny application bundled with
+#' the package.
+#'
+#' @export
+#' @importFrom shiny runApp
+#'
+#' @examples
+#' \dontrun{
+#'   run_app()
+#' }
+run_app <- function() {
+   # Find the app's directory within the installed package
+   app_dir <- system.file("shiny_app", package = "RMSTdesign")
+
+   # Check if the directory exists
+   if (app_dir == "") {
+      stop(
+         "Could not find the app directory. ",
+         "Try re-installing the `RMSTdesign-Package`.",
+         call. = FALSE
+      )
+   }
+
+   # Launch the app
+   shiny::runApp(app_dir, display.mode = "normal")
+}
