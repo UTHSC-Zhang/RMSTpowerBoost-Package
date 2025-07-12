0ea63f986fdd144e5bebb18253b8ec795e009b44
Arnab Aich <118152677+arnabaich96@users.noreply.github.com>
2025-07-12 12:13:26 -0500

    Fix nested repository and commit app files directly

--- R/run_app.R
+++ R/run_app.R
@@ -12,13 +12,9 @@
 #'   RMSTSS::run_app()
 #' }
 run_app <- function() {
-   # First, load the package itself to make all its functions available
-   # to the Shiny app's environment. This is a crucial step.
    library(RMSTSS)
-
    # Get the path to the Shiny app directory within the installed package
    app_dir <- system.file("shiny_app", package = "RMSTSS")
-
    # Check if the directory exists
    if (app_dir == "") {
       stop(
